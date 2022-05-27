22ce717acfbad904ef388348b8cb03f923387d6e
Jason Kobuchi <jekobuchi@gmail.com>
2022-05-26 22:34:29 -0700

    added sidebar menu functionality

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import brandIcon from '../../images/logo.svg';
 import cartIcon from '../../images/icon-cart.svg';
 import './Navbar.css';
 
 const Navbar = () => {
 
-    const openNav = () => {
-        document.getElementById("sidebar").style.width = "250px";
-        document.getElementById("sidebar-button").style.marginLeft = "250px";
-    }
-      
-    const closeNav = () => {
-        document.getElementById("sidebar").style.width = "0";
-        document.getElementById("sidebar-button").style.marginLeft= "0";
-    }
+    const [ sideCollapsed, setSideCollapsed ] = useState(false);
+
+    const toggleSideCollapsed = useCallback(() => {
+      console.log('toggle')
+      setSideCollapsed(collapsed => !collapsed)
+    }, [ setSideCollapsed ])
 
     return(
         <div className='navbar-container'>
-            <div id="sidebar" className='sidebar-menu'>
-                <a href="javascript:void(0)" className="closebtn" onclick={closeNav}>×</a>
+            <div id="sidebar" className='sidebar-menu' onClick={toggleSideCollapsed} collapsed={sideCollapsed}>
                 <a>Collections</a>
                 <a>Men</a>
                 <a>Women</a>
@@ -26,7 +22,7 @@ const Navbar = () => {
                 <a>Contact</a>
             </div>
             <div id="open-button">
-                <button className="openbtn" onclick={openNav}>☰</button>
+                <button className="openbtn">☰</button>
             </div>
             <div className='navbar-brand'>
                 <img src={brandIcon} alt='brand-logo'></img>
